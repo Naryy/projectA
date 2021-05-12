7142d19ac35c7e246fb470bc0e4b859d75c83e22
“Naryy” <narikawanariyuki@gmail.com>
2021-05-12 13:03:08 +0900

    first

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCirzzMbODiY66ZS5V3NntKhPWiP20ruZI",
+    authDomain: "project-a-th.firebaseapp.com",
+    projectId: "project-a-th",
+    storageBucket: "project-a-th.appspot.com",
+    messagingSenderId: "822630599014",
+    appId: "1:822630599014:web:cc084613ba9bec328020a7",
+    measurementId: "G-MBCFESGE96"
+  };
+  firebaseConfig.initializeApp(firebaseConfig);
